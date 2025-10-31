71d46f022b692c1d6bd4b5a69697f70457884253
josh <105252972+itxjoshy@users.noreply.github.com>
2025-10-31 14:41:57 +0100

    Rename CSS import to lowercase

--- src/assets/pages/adminLogin.jsx
+++ src/assets/pages/adminLogin.jsx
@@ -5,7 +5,7 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import "./AdminLogin.css";
+import "./adminlogin.css";
 import logo from "../logo.png";
 
 export default function AdminLogin() {

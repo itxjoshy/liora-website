3d81979db9513ff03226caf86e4781bb0616a129
itxjoshy <johnmanifest2017@gmail.com>
2025-10-31 14:29:26 +0100

    fixec css case sensitivity in import

--- src/assets/pages/adminLogin.jsx
+++ src/assets/pages/adminLogin.jsx
@@ -5,7 +5,7 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import "./adminLogin.css";
+import "./adminlogin.css";
 import logo from "../logo.png";
 
 export default function AdminLogin() {
